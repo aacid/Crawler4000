f2c4255ce08287396f9fc3c4167c0894f3315a8c
aacid <olivercern@gmail.com>
2016-04-17 21:27:41 +0200

    - added table Friends to definition - added addPerson()

--- Crawler4000/Crawler4000/Crawler4000.py
+++ Crawler4000/Crawler4000/Crawler4000.py
@@ -2,7 +2,7 @@ from ConfigManager import ConfigManager
 from DBManager import DBManager
 class Crawler4000(object):
     def __init__(self):
-        config = ConfigManager()
-        db = DBManager()
+        self.config = ConfigManager()
+        self.db = DBManager()
 
 diplo = Crawler4000()
--- Crawler4000/Crawler4000/DBManager.py
+++ Crawler4000/Crawler4000/DBManager.py
@@ -2,7 +2,9 @@ import sqlite3
 
 class DBManager(object):
     """manages all interaction with DB"""
-    tables = ['People',]
+
+    tables = ['People', 'Friends']
+
     def __init__(self):
         self.conn = sqlite3.connect("data.db")
         self.c = self.conn.cursor()
@@ -12,6 +14,7 @@ class DBManager(object):
     def __del__(self):
         self.conn.commit()
         self.c.close()
+
     def createTable(self, name):
         if name == 'People':
             self.c.execute("""CREATE TABLE `People` (
@@ -21,8 +24,25 @@ class DBManager(object):
                                 `FriendsScrapped`	TEXT DEFAULT 'N',
                                 PRIMARY KEY(id)
                             );""")
+        elif name == 'Friends':
+            self.c.execute("""CREATE TABLE `Friends` (
+                                `Person`	TEXT NOT NULL,
+                                `Friend`	TEXT NOT NULL,
+                                PRIMARY KEY(Person,Friend)
+                            );""")
+
     def checkConsistency(self):
         for table in self.tables:
             self.c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", (table,))
             if self.c.fetchone() == None:
                 self.createTable(table)
+
+
+    def addPerson(self, id, name):
+        try:
+            self.c.execute("INSERT INTO People VALUES (?, ?)", (id, name))
+        except sqlite3.IntegrityError:
+            raise sqlite3.IntegrityError
+
+                
+            
