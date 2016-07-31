194403e66666ee5d365731ae2f1f3d586e79b4b1
aacid <olivercern@gmail.com>
2016-07-31 15:44:25 +0200

    - added full name to config

--- Crawler4000/Crawler4000/Source/ConfigManager.py
+++ Crawler4000/Crawler4000/Source/ConfigManager.py
@@ -7,17 +7,32 @@ class ConfigManager(object):
     FB_SECTION = 'FacebookLogin'
     FB_USERNAME = 'username'
     FB_PASSWORD = 'password'
+    FB_FULLNAME = 'fullname'
+
+    config_read = False
+
     def __init__(self):        
-        self.config = ConfigParser.ConfigParser()
-        if os.path.isfile(self.MAIN_FILE):
-            self.config.read(self.MAIN_FILE)
-        else:
-            self.createConfig()
+        print 'Reading data from main.cfg.'
+        try:       
+            self.config = ConfigParser.ConfigParser()
+            if os.path.isfile(self.MAIN_FILE):
+                self.config.read(self.MAIN_FILE)
+            else:
+                self.createConfig()
+        except:
+            print 'Could not read data from main.cfg.'
+            return
+
+        self.config_read = True
+    
+    def isLoaded(self):
+        return self.config_read
             
     def createConfig(self):
         self.config.add_section(self.FB_SECTION)
         self.config.set(self.FB_SECTION, self.FB_USERNAME, '_DUMMY_USER_')
         self.config.set(self.FB_SECTION, self.FB_PASSWORD, '_DUMMY_PASS_')
+        self.config.set(self.FB_SECTION, self.FB_FULLNAME, '_DUMMY_NAME_')
         
         with open(self.MAIN_FILE, 'wb') as configfile:
             self.config.write(configfile)
@@ -29,3 +44,6 @@ class ConfigManager(object):
     def getFBPassword(self):
         if self.config.has_option(self.FB_SECTION, self.FB_PASSWORD):
             return self.config.get(self.FB_SECTION, self.FB_PASSWORD)
+    def getFBFullname(self):
+        if self.config.has_option(self.FB_SECTION, self.FB_FULLNAME):
+            return self.config.get(self.FB_SECTION, self.FB_FULLNAME)
